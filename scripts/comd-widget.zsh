599580cb9bd9ba21c9ba1f5bd4b77bcadb665278
nic <christoph.niederer99@gmail.com>
2026-04-15 16:58:17 +0200

    refactor: Cleanup Source script

--- scripts/comd-widget.zsh
+++ scripts/comd-widget.zsh
@@ -8,46 +8,32 @@
 #   export COMD_CMD='comd'     # binary or full command line (zsh-split)
 #   export COMD_ARGS='--foo'   # extra args (zsh-split)
 
-comd() {
-  emulate -L zsh
-  setopt localoptions pipefail no_aliases 2>/dev/null
-
-  local tmp bin
-  tmp=$(mktemp) || return 1
-  bin=${COMD_CMD:-comd}
-  local cmdword="${bin%% *}"
-  if [[ ! -x ${cmdword} ]] && ! command -v -- "$cmdword" &>/dev/null; then
-    print -u2 "comd-widget: command not found: $cmdword (set COMD_CMD?)"
-    command rm -f "$tmp"
-    return 1
-  fi
-
-  local -a run
-  run=(${(z)bin})
-  [[ -n ${COMD_ARGS-} ]] && run+=(${(z)COMD_ARGS})
-
-  COMD_ZSH_BUFFER_FILE=$tmp command -- "${run[@]}"
-  local r=$?
-
-  if [[ -s $tmp ]]; then
-    print -z "$(<$tmp)"
-  fi
-  command rm -f "$tmp"
-  return r
-}
-
 comd-widget() {
   emulate -L zsh
-  setopt localoptions no_aliases 2>/dev/null
+  setopt localoptions pipefail no_aliases 2>/dev/null
 
   if ! [[ -o interactive ]] || ! [[ -o zle ]]; then
     print -u2 "comd-widget: needs an interactive zsh with ZLE"
     return 1
   fi
 
-  zle .push-line
-  BUFFER=comd
-  zle .accept-line
+  local tmp bin
+  tmp=$(mktemp) || return 1
+  if [[ ! -x "comd" ]] && ! command -v -- "comd" &>/dev/null; then
+    print -u2 "comd-widget: command not found: comd"
+    command rm -f "$tmp"
+    return 1
+  fi
+
+  COMD_ZSH_BUFFER_FILE=$tmp command -- "comd"
+  local r=$?
+
+  if [[ -s $tmp ]]; then
+    print -z "$(<$tmp)"
+    zle .accept-line
+  fi
+  command rm -f "$tmp"
+  return r
 }
 
 zle -N comd-widget
